b7871c5bf575a4b45b346e9714440d9a683770a3
induwaraR98 <icredu98@gmail.com>
2025-12-15 00:46:10 +0530

    integrate backend and frontend in technician popup

--- src/sessions/dto/update-session.dto.ts
+++ src/sessions/dto/update-session.dto.ts
@@ -1,4 +1,3 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { CreateSessionDto } from './create-session.dto';
 
-export class UpdateSessionDto extends PartialType(CreateSessionDto) {}
+export class UpdateSessionDto extends CreateSessionDto {}
